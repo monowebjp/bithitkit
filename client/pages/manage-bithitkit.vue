89da855225cd88219d10e7861f40cdd3ea1d2190
monowebjp <capybara@monoweb.jp>
2022-02-08 21:57:52 +0900

    ログイン画面フロント作成、共通sass対応

---
+++ client/pages/manage-bithitkit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button>ログインボタン</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  box-sizing: content-box;
+  max-width: convertRem(640);
+  padding: 0 convertRem(22);
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+button {
+  display: block;
+  width: 100%;
+  height: convertRem(80);
+  background: $primaryBtnBgColor;
+  color: $primaryBtnFontColor;
+  border-radius: convertRem(8);
+}
+</style>
